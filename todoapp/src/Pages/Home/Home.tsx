4b91e3c0dd2849c50c7bc97128f8377ef425c3fd
aszeliga24 <adrian.sz@o2.pl>
2023-04-05 16:47:25 +0200

    AppBar component

--- todoapp/src/Pages/Home/Home.tsx
+++ todoapp/src/Pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from 'Components/Header/Header'
+import AppBar from 'Components/AppBar/AppBar'
 
 const Home = () => {
 	return (
 		<div className='homepage__container'>
 			<Header title='homepage' />
-			rest goes here
+
+			<AppBar />
 		</div>
 	)
 }
